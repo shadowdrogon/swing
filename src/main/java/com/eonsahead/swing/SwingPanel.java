488578f272c4d8ee0f15db5b9c9b5ccb543a2a7d
Leon Hannah Tabak <l.tabak@ieee.org>
2020-04-02 10:07:42 -0500

    feat: Change motion of circle.
    The circle now looks like a ball bouncing back and forth
    between the floor and ceiling.

--- src/main/java/com/eonsahead/swing/SwingPanel.java
+++ src/main/java/com/eonsahead/swing/SwingPanel.java
@@ -1,4 +1,3 @@
-
 package com.eonsahead.swing;
 
 import java.awt.Color;
@@ -14,66 +13,67 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class SwingPanel extends JPanel implements ActionListener {
-    
+
     private double centerX = 0.0;
     private double centerY = 0.0;
     private double radius = 0.5;
+    private double deltaY = 0.02;
     private Color color = Color.red;
-    
-    public SwingPanel() { 
-        Timer timer = new Timer( 50, this );
+
+    public SwingPanel() {
+        Timer timer = new Timer(50, this);
         timer.start();
     } // SwingPanel()
-    
+
     public double getCenterX() {
         return this.centerX;
     } // getCenterX()
-    
-    public void setCenterX( double x ) {
+
+    public void setCenterX(double x) {
         this.centerX = x;
     } // setCenterX( double )
-    
+
     public double getCenterY() {
         return this.centerY;
     } // getCenterY()
-    
-    public void setCenterY( double y ) {
+
+    public void setCenterY(double y) {
         this.centerY = y;
     } // setCenterY( double )
-    
+
     public double getRadius() {
         return this.radius;
     } // getRadius()
-    
-    public void setRadius( double r ) {
+
+    public void setRadius(double r) {
         this.radius = r;
     } // setRadius( double )
-    
+
     public Color getColor() {
         return this.color;
     } // getColor()
-    
-    public void setColor( Color c ) {
+
+    public void setColor(Color c) {
         this.color = c;
     } // setColor( Color )
-    
+
     @Override
-    public void paintComponent( Graphics g ) {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2D = (Graphics2D) g;
-        
+
         int w = this.getWidth();
         int h = this.getHeight();
-        
+
         AffineTransform transform = new AffineTransform();
         AffineTransform scaling = new AffineTransform();
-        scaling.setToScale(w/2, h/2 );
+        scaling.setToScale(w / 2, h / 2);
         AffineTransform translation = new AffineTransform();
         translation.setToTranslation(1.0, 1.0);
-        
-        transform.concatenate( scaling );
-        transform.concatenate( translation );
-        
+
+        transform.concatenate(scaling);
+        transform.concatenate(translation);
+
         // Replace this block of code that creates
         // an ellipse with your own code that draws
         // something else
@@ -82,27 +82,31 @@ public class SwingPanel extends JPanel implements ActionListener {
         double d = 2 * this.radius;
         double ulx = this.centerX - this.radius;
         double uly = this.centerY - this.radius;
-        Ellipse2D.Double circle = new Ellipse2D.Double( ulx, uly, d, d );
-        
+        Ellipse2D.Double circle = new Ellipse2D.Double(ulx, uly, d, d);
+
         Shape shape = transform.createTransformedShape(circle);
-        g2D.setColor( Color.red );
-        g2D.fill( shape );    
+        g2D.setColor(this.getColor());
+        g2D.fill(shape);
     } // paintComponent( Graphics )
 
     @Override
-    public void actionPerformed(ActionEvent event ) {
+    public void actionPerformed(ActionEvent event) {
         // You might also like to try what happens
         // in each step of the animation
         // Move? In which direction? How much?
         // Make bigger? Or make smaller?
         // Rotate? (There's an AffineTransform for that, too.)
         // Change color?
-        
-        this.centerY = this.centerY + 0.02;
-        if( this.centerY > 0.5 ) {
-            this.centerY = -0.5;
+
+        if (this.centerY > 0.5) {
+            this.deltaY = -this.deltaY;
         } // if
+        else if (this.centerY < -0.5) {
+            this.deltaY = -this.deltaY;
+        } // else if
+        this.centerY += this.deltaY;
+
         this.repaint();
     } // actionPerformed( ActionEvent )
-    
+
 } // SwingPanel
